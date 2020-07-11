fb00f0bb518b722e39e9ddfc5a27184691816239
Tomomasa UCHIDA <tomomasa.is.0930@gmail.com>
2020-07-12 00:39:35 +0900

    added

---
+++ py/calc_r2-score_M50.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+
+# Check arguments
+import sys
+args = sys.argv
+if len(args) != 2:
+    sys.exit()
+
+# Read csv file
+# csv = pd.read_csv(args[1], header=None)
+csv = pd.read_csv(args[1])
+
+# Convert to numpy array
+csv = csv.values
+print(csv.shape)
+print(csv)
+
+# Get each column
+L       = csv[:,0]
+M_mean  = csv[:,1]
+M_max   = csv[:,2]
+M_min   = csv[:,3]
+M_std   = csv[:,4]
+
+# Get the index that meets the conditions
+idx = np.where(M_mean<=50)
+idx = np.append(idx, np.max(idx)+1)
+# idx = np.append(idx, np.max(idx))
+print(idx)
+
+# Calc. r2_score
+from sklearn.metrics import r2_score
+alpha = 0.5
+# y_true = M_mean[idx]
+y_true = M_mean[idx] - M_std[idx]
+y_pred = M = alpha * L[idx]
+print("\nr2_score:", round(r2_score(y_true, y_pred),2) )
+
+# >>> from sklearn.metrics import r2_score
+# >>> y_true = [3, -0.5, 2, 7]
+# >>> y_pred = [2.5, 0.0, 2, 8]
+# >>> r2_score(y_true, y_pred)
+# 0.948...
---
+++ py/create_M50_figure.py
@@ -0,0 +1,62 @@
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+plt.style.use('bmh')
+plt.rcParams["mathtext.fontset"] = "cm"
+plt.rcParams["mathtext.rm"] = "Times New Roman"
+plt.rcParams["font.size"] = 14
+
+# Check arguments
+import sys
+args = sys.argv
+if len(args) != 2:
+    sys.exit()
+
+# Read csv file
+# csv = pd.read_csv(args[1], header=None)
+csv = pd.read_csv(args[1])
+
+# Convert to numpy array
+csv = csv.values
+print(csv.shape)
+print(csv)
+
+# Get each column
+L       = csv[:,0]
+M_mean  = csv[:,1]
+M_max   = csv[:,2]
+M_min   = csv[:,3]
+M_std   = csv[:,4]
+
+# Get the index that meets the conditions
+idx = np.where(M_mean<=50)
+# idx = np.append(idx, np.max(idx)+1)
+# idx = np.append(idx, np.max(idx))
+print(idx)
+
+# Draw a linear function(Theoretical linear function)
+alpha = 0.5
+L_R = np.arange(1, 100, 1)
+M = alpha * L_R
+plt.plot(L_R, M, "--", color='red', label=r"$M= \alpha L (\alpha=0.5)$")
+
+# M
+# plt.scatter(L, M_max, color='black', label=r"$M_\mathrm{max}$", marker="^")
+# plt.scatter(L, M_mean, color='black', label=r"$M_\mathrm{mean}$", marker="o")
+# plt.scatter(L, M_min, color='black', label=r"$M_\mathrm{min}$", marker="v")
+
+# Draw error bar
+plt.errorbar(L[idx], M_mean[idx], M_std[idx], fmt='ok', elinewidth=1, capsize=3, ecolor='black')
+
+# plt.grid()
+plt.legend(fontsize=14)
+plt.gca().set_aspect('equal')
+
+# plt.xticks([1, 20, 40, 60, 80, 100], fontsize=14)
+plt.xlabel('$L$', fontsize=14)
+# plt.yticks([0, 20, 40, 60, 80, 100], fontsize=14)
+# plt.yticks([0, 10, 20, 30, 40, 50], fontsize=14)
+plt.ylabel('$M$', fontsize=14)
+
+plt.show()
